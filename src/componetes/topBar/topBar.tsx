6cd093db71267a56faa382277552a166d278d5c8
Joao <joaopedro767767@gmail.com>
2022-05-07 08:40:43 -0300

    Segundo commit eCommerce

---
+++ src/componetes/topBar/topBar.tsx
@@ -0,0 +1,11 @@
+// import classNames from "classnames";
+import React from "react";
+export default class topBar extends React.Component<any, {}> {
+  render() {
+    return (
+      <>
+        <div className="topBar"></div>
+      </>
+    );
+  }
+}
