ddce31e6b09170d3d6c2d047ef9fb6fd9c876592
Joao <joaopedro767767@gmail.com>
2022-05-07 11:08:18 -0300

    Top Bar

--- src/componetes/topBar/topBar.tsx
+++ src/componetes/topBar/topBar.tsx
@@ -1,10 +1,29 @@
 // import classNames from "classnames";
 import React from "react";
-export default class topBar extends React.Component<any, {}> {
+export default class TopBar extends React.Component<any, {}> {
   render() {
     return (
       <>
-        <div className="topBar"></div>
+        <div className="topbar-content">
+          <div className="topbar-left">
+            <div className="topbar-logo">
+              <i className="fas fa-home"></i>
+            </div>
+            <div className="topbar-sell">Sell here</div>
+            <div className="topbar-register">Register</div>
+          </div>
+          <div className="topbar-center">
+            <div className="topbar-search"></div>
+            <div className="topbar-custumer">Consumer Eletronics</div>
+          </div>
+          <div className="topbar-right">
+            <div className="topbar-singin">Sing In</div>
+            <div className="topbar-cart">My cart</div>
+            <div className="topbar-icon">
+              <img src="https://picsum.photos/200/300" />
+            </div>
+          </div>
+        </div>
       </>
     );
   }
