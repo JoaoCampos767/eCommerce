233135fa3f6c6302ec4a898c5ad759b6a085244d
João Pedro Campos <joaopedro767767@gmail.com>
2022-05-21 10:13:37 -0300

    Add Icons

--- src/componetes/subMenu/subMenu.tsx
+++ src/componetes/subMenu/subMenu.tsx
@@ -11,15 +11,60 @@ export default class SubMenu extends React.Component<any, {}> {
               <i className="fas fa-tshirt" />
             </div>
           </div>
-          <div className="submenu-musics">Musics</div>
-          <div className="submenu-sports">Sports</div>
-          <div className="submenu-pets">Pets</div>
-          <div className="submenu-acessories">Acessories</div>
-          <div className="submenu-equipment">Equipment</div>
-          <div className="submenu-garden">Garden</div>
-          <div className="submenu-electrical">Electrical</div>
-          <div className="submenu-toys">Toys</div>
-          <div className="submenu-vintage">Vintage</div>
+          <div className="submenu-musics">
+            Musics
+            <div className="submenu-icon-musics">
+              <i className="fas fa-music" />
+            </div>
+          </div>
+          <div className="submenu-sports">
+            Sports
+            <div className="submenu-icon-sports">
+              <i className="fas fa-futbol"></i>
+            </div>
+          </div>
+          <div className="submenu-pets">
+            Pets
+            <div className="submenu-icon-pets">
+              <i className="fas fa-bone"></i>
+            </div>
+          </div>
+          <div className="submenu-acessories">
+            Acessories
+            <div className="submenu-icon-acessories">
+              <i className="fas fa-cut"></i>
+            </div>
+          </div>
+          <div className="submenu-equipment">
+            Equipment
+            <div className="submenu-icon-equipment">
+              <i className="fas fa-binoculars"></i>
+            </div>
+          </div>
+          <div className="submenu-garden">
+            Garden
+            <div className="submenu-icon-garden">
+              <i className="fas fa-leaf"></i>
+            </div>
+          </div>
+          <div className="submenu-electrical">
+            Electrical
+            <div className="submenu-icon-eletrical">
+              <i className="fas fa-bolt"></i>
+            </div>
+          </div>
+          <div className="submenu-toys">
+            Toys
+            <div className="submenu-icon-toys">
+              <i className="fas fa-gamepad"></i>
+            </div>
+          </div>
+          <div className="submenu-vintage">
+            Vintage
+            <div className="submenu-icon-vintage">
+              <i className="fas fa-compact-disc"></i>
+            </div>
+          </div>
         </div>
       </>
     );
